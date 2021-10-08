83acf8f73c11c72b96dd9f67969db0a436ace0bc
zxy1127 <2960699195@qq.com>
2021-10-08 22:52:39 +0800

    try

---
+++ eclipse/ComputerArea/src/ComputerArea/ComputerArea.java
@@ -0,0 +1,14 @@
+package ComputerArea;
+
+public class ComputerArea {
+
+	public static void main(String[] args) {
+		double radius;
+		double area;
+		radius=20;
+		area=radius*radius*3.14159;
+		System.out.println("The area for the circle of radius " + radius + " is " + area);
+
+	}
+
+}
