a6851cc048eb047ed85cc09e50f9122c4cbcef12
zxy1127 <2960699195@qq.com>
2021-10-08 22:40:38 +0800

    try

---
+++ eclipse/ShowLogicErrors/src/ShowLogicErrors/ShowLogicErrors.java
@@ -0,0 +1,11 @@
+package ShowLogicErrors;
+
+public class ShowLogicErrors {
+
+	public static void main(String[] args) {
+		System.out.print("Celsius 35 is Fahrenheit degree");
+		System.out.println((9/5)*35+32);
+
+	}
+
+}
