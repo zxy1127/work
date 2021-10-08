87bf82e61faec91761bad02968b705d669f09e78
zxy1127 <2960699195@qq.com>
2021-10-08 16:37:10 +0800

    try

---
+++ eclipse/ComputerExpression/src/ComputerExpression/ComputerExpression.java
@@ -0,0 +1,10 @@
+package ComputerExpression;
+
+public class ComputerExpression {
+
+	public static void main(String[] args) {
+		System.out.println("(10.5+2*3)/(45-3.5)=");
+		System.out.println((10.5+2*3)/(45-3.5));
+	}
+
+}
